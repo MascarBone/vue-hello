997f802d61ee57b04b3a03f12e5ceb1bc6fd46af
MascarBone <simone.bonelli@gmail.com>
2021-09-14 15:42:40 +0200

    H1 and IMG with VUE implementation

--- js/script.js
+++ js/script.js
@@ -1,2 +1,15 @@
 // Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 // Bonus: Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+let app = new Vue ({
+    
+    el : '#data',
+
+    data : {
+        message: 'Welcome to the new interface using VUE\'s Framework',
+        myClasses: 'py-3',
+        imgLink: 'https://c.tenor.com/pjLsAVyJPwwAAAAd/diablo-ii-remaster-resurrected.gif'
+    },
+
+    
+});
