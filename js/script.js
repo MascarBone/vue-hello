b1fa87935a3b39e58b8053affc2178f8ee2c27d2
MascarBone <simone.bonelli@gmail.com>
2021-09-14 16:28:53 +0200

    Tried a change of color with a css variable

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 // Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 // Bonus: Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+// let coloredColor = '#FFAA00';
+// var cssVar = document.querySelector(':root')
+// cssVar.style.setProperty('--colorHex', coloredColor);
 
 let app = new Vue ({
     
@@ -11,12 +14,22 @@ let app = new Vue ({
         imgLink: 'https://c.tenor.com/pjLsAVyJPwwAAAAd/diablo-ii-remaster-resurrected.gif',
         lookhere: `THERE'S SOMETHING HERE`,
         clickedColor: '',
-        fontSize: 40
+        fontSize: 40,
+        cssVar: document.querySelector(':root')
     },
 
     methods: {
         generateColor: function() {
             this.clickedColor = "#" + Math.floor(Math.random()* 16777215).toString(16);
+        },
+
+        generateHexColor: function() {
+            return "#"+Math.floor(Math.random()* 16777215).toString(16);
+        },
+
+        outputConsole: function() {
+            console.log(this.cssVar);
+            this.cssVar.style.setProperty('--colorHex', this.generateHexColor());
         }
 
     }
